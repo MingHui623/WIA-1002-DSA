fbbae5c71afc1a1efb323b772e38da045e75cde2
MingHui623 <24004540@siswa.um.edu.my>
2025-03-17 11:35:36 +0800

    Update T1Q1.java

--- Tutorial-Programming Fundamental/T1Q1.java
+++ Tutorial-Programming Fundamental/T1Q1.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Ming Hui
- */
 public class T1Q1 {
     public static void main(String[] args) {
         //instantiate 5 telephone objects
